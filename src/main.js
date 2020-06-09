f6d7561fc588428be21fe59275b5eba103e81a79
sunyu <2535675241@qq.com>
2020-06-09 21:53:54 +0800

    使用了element-ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 注册element-ui
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
